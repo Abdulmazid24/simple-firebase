ab136b6c08eef79146a463a76dfa97e6fc673813
Abdulmazid24 <a.m.abdulmazid24@gmail.com>
2024-06-18 20:31:31 +0600

    explore more some in firebase

--- src/__steps.js
+++ src/__steps.js
@@ -5,5 +5,12 @@
  * 4.install firebase : npm install firebase
  * 5.add config file to your project
  * 6.DANGER: Do not publish or make firebase config to public by pushing those to github
- *visite: Go to docs > Build > Authentication > Web > Get Started
+ *7. visite: Go to docs > Build > Authentication > Web > Get Started
+ 8.export app from the firebase.config.js file: export default app;
+ 9.Login.jsx :import getAuth from firebase/auth 
+ 10.create const auth = getAuth(app)
+ 11. import GoogleAuthProvider and create a new provider
+ 12.use signInWithPopup and pass auth and provider
+ 13.activate sign-in method (google,facebook,github, etc)
+ 14.
  */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,19 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, signInWithPopup } from 'firebase/auth';
 import app from '../../firebase/firebase.init';
 import { GoogleAuthProvider } from 'firebase/auth/web-extension';
 
 const Login = () => {
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
-
   const handleGoogleSignIn = () => {
-    console.log('google mama is coming');
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
   };
   return (
     <div>
